f781b82c0a71b808f106b26bdc138dc30c5bfcb0
nicearts44 <144501750+nicearts44@users.noreply.github.com>
2025-04-06 18:26:03 +0100

    Update Ardmind.ino
    Comment of the latest source code project

--- Ardmind.ino
+++ Ardmind.ino
@@ -1,114 +1,111 @@
-const int red_led=1;
-const int green_led=2;
-const int blue_led=3;
+// Define LED pins
+const int red_led = 1;
+const int green_led = 2;
+const int blue_led = 3;
 
-const int red_button=8;
-const int green_button=9;
-const int blue_button=10;
+// Define button pins
+const int red_button = 8;
+const int green_button = 9;
+const int blue_button = 10;
 
-const int leds[3]={1,2,3};
-const int potentio=0;
-int tension= 0;
+// Array to store LED pins for iteration
+const int leds[3] = {1, 2, 3};
 
+// Potentiometer analog input pin
+const int potentio = 0;
+int tension = 0; // Variable to store analog reading
+
+// Variables to store button states
 int red_state;
 int green_state;
 int blue_state;
 
 void setup() {
- pinMode(red_led,OUTPUT);
- pinMode(green_led,OUTPUT);
- pinMode(blue_led,OUTPUT);
+  // Initialisation of LED pins as OUTPUT
+  pinMode(red_led, OUTPUT); // Defining the pin 1 (red LED ) as an OUTPUT
+  pinMode(green_led, OUTPUT); // Defining the pin 2 ( green LED ) as an OUTPUT
+  pinMode(blue_led, OUTPUT);  // Defining the pin 3 (blue LED ) as OUTPUT
 
- pinMode(red_button,INPUT);
- pinMode(green_button,INPUT);
- pinMode(blue_button,INPUT);
+  // Initialisation of buttons pins as INPUT
+  pinMode(red_button, INPUT);   // Defining the pin 8 (red Button ) as an INPUT
+  pinMode(green_button, INPUT); // Defining the pin 9 (green Button ) as an INPUT
+  pinMode(blue_button, INPUT);  // Defining the pin 10 (red Button ) as an INPUT
 
- int i=0;
- for(i=0;i<3;i++){
-  pinMode(leds[i],OUTPUT);
-  digitalWrite(leds[i],HIGH);
- }
- 
- 
+  // Turn all LEDs OFF at the beginning
+  for (int i = 0; i < 3; i++) {
+    pinMode(leds[i], OUTPUT);
+    digitalWrite(leds[i], HIGH); // HIGH = OFF because wired with pull-down logic
+  }
 }
 
 void loop() {
-  
-  red_state=0;
-  green_state=0;
-  blue_state=0;
+  // Reseting button states
+  red_state = 0;
+  green_state = 0;
+  blue_state = 0;
 
-  red_state=digitalRead(red_button);
-  green_state=digitalRead(green_button);
-  blue_state=digitalRead(blue_button);
-  
-  if (red_state==1){
-    digitalWrite(red_led,LOW);
-  }
-  else{
-    digitalWrite(red_led,HIGH);
-  }
+  // Reading button states
+  red_state = digitalRead(red_button);     // Reading the state of the red button
+  green_state = digitalRead(green_button); // Reading the state of the green button
+  blue_state = digitalRead(blue_button);   // Reading the state of the blue button
 
-  if (green_state==1){
-    digitalWrite(blue_led,LOW);
-    delay(1000);
-    digitalWrite(blue_led,HIGH);
-
-    digitalWrite(red_led,LOW);
-    delay(1000);
-    digitalWrite(red_led,HIGH);
-
-    digitalWrite(green_led,LOW);
-    delay(1000);
-    digitalWrite(blue_led,HIGH);
-    delay(1000);
-
-    digitalWrite(green_led,LOW);
-    digitalWrite(red_led,LOW);
-    digitalWrite(blue_led,LOW);
-    delay(1000);
-    
-  }
-  else{
-    digitalWrite(red_led,HIGH);
-    digitalWrite(blue_led,HIGH);
-    digitalWrite(green_led,HIGH);
+  // Controling Red LED
+  if (red_state == 1) {
+    digitalWrite(red_led, LOW); // Turn ON red LED if red button pressed
+  } else {
+    digitalWrite(red_led, HIGH); // Turn OFF red LED when red button is not pressed
   }
 
-  if (blue_state==1){
-    digitalWrite(blue_led,LOW);
+  // Special light sequence which will been executing when green button is pressed
+  if (green_state == 1) {
+    digitalWrite(blue_led, LOW);  // Turn ON blue LED 
+    delay(1000);                  // Wait for a second
+    digitalWrite(blue_led, HIGH); // Turn OFF blue LED
+
+    digitalWrite(red_led, LOW);   // Turn ON red LED
+    delay(1000);                  // Wait for a second
+    digitalWrite(red_led, HIGH);  // Turn OFF red LED
+
+    digitalWrite(green_led, LOW); // Turn ON green LED
+    delay(1000);                  // Wait for a second
+   
+    digitalWrite(blue_led, HIGH); // Turn ON blue again
+    delay(1000);                  // Wait for a second
+
+    // Turn ON all LEDs
+    digitalWrite(green_led, LOW); // Turn ON green LED 
+    digitalWrite(red_led, LOW);   // Turn ON red LED
+    digitalWrite(blue_led, LOW);  // Turn ON blue LED
+    delay(1000);                  // Wait for a second
+  } else {
+    // Turn OFF all LEDs 
+    digitalWrite(red_led, HIGH);  // Turn OFF red LED
+    digitalWrite(blue_led, HIGH); // Turn OFF blue LED
+    digitalWrite(green_led, HIGH);// Turn OFF green LED
   }
-  else{
-    digitalWrite(blue_led,HIGH);
+
+  // Controling Blue LED
+  if (blue_state == 1) {
+    digitalWrite(blue_led, LOW); // Turn ON blue LED if blue button pressed
+  } else {
+    digitalWrite(blue_led, HIGH); // Turn OFF blue LED when blue button is not pressed
   }
 
-tension=analogRead(potentio);
-teinte(tension);
+  // Read potentiometer value
+  tension = analogRead(potentio); // Reading the value on the tension on the analog pin A0
 
-
+  // Call function to adjust LED state based on potentiometer
+  teinte(tension);
 }
 
-
-void teinte(int valeur)
-{
-  int i;
-  for(i=0; i<3 ; i++)
-  {
-    if(valeur >= (i*10))
-    digitalWrite(leds[i-1], LOW);
-    else
-    digitalWrite(leds[i-1],HIGH);
+// Function to update LED states based on analog value
+void teinte(int value) {
+  for (int i = 0; i < 3; i++) {
+   if (value >= (i * 10))
+   {
+  digitalWrite(leds[i], LOW); // Turn ON the LEDs according to the value of the potentiometer 
+   } 
+   else {
+         digitalWrite(leds[i], HIGH);
+        }
   }
-
-    // if (valeur < 100){
-    //   digitalWrite(leds[i], LOW);
-    // }
-      
-    
-  //   else if (valeur ==200 ){
-  //     digitalWrite(leds[i], LOW);
-  //   }
-  //   else 
-  //   digitalWrite(leds[i], LOW);
-  // }
-}
